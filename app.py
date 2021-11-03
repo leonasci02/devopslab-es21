26699895a85d4505405acdb12c57cb25e6d81c1e
Leonardo Nascimento <leonardo.nascimentoo02@hotmail.com>
2021-11-03 19:49:01 -0300

    CSRF

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)  
+
 @app.route("/")
 def pagina_inicial():
-    return "Customizei a mensagem - Leonardo Nascimento Azogue 13/10/2021"
+    return "Customizei a mensagem - Leonardo Nascimento Azogue 03/11/2021"
 
 if __name__ == '__main__':
     app.run(debug=True)
